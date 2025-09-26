a75250ece089f75b5f64a5fbf6950f0821421360
Zhang Dawei <dawei.zhang@my.jcu.edu.au>
2025-09-26 17:08:34 +0800

    Add get_score_status function

--- score.py
+++ score.py
@@ -3,12 +3,24 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    result = get_score_status(score)
+    return(result)
+
+    random_score = random.uniform(0, 100)
+    return("Random score:", random_score, "Result:", get_score_status(random_score))
+
+def get_score_status(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main()
