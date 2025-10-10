c1f8fb407f062c105cbb466b98670e446cd45d6a
Zhang Dawei <dawei.zhang@my.jcu.edu.au>
2025-10-10 10:40:32 +0800

    Adding my answer as a comment to GitHub.

--- Prac_04/lists_warmup.py
+++ Prac_04/lists_warmup.py
@@ -1,12 +1,14 @@
+# Warm-Up Exercise
+# File: lists_warmup.py
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0]
-numbers[-1]
-numbers[3]
-numbers[:-1]
-numbers[3:4]
-5 in numbers
-7 in numbers
-"3" in numbers
-numbers + [6, 5, 3]
+# numbers[0]                 3
+# numbers[-1]                2
+# numbers[3]                 1
+# numbers[:-1]               [3,1,4,1,5,9]
+# numbers[3:4]               [1]
+# 5 in numbers               True
+# 7 in numbers               False
+# "3" in numbers             False
+# numbers + [6, 5, 3]        [3,1,4,1,5,9,2,6,5,3]
