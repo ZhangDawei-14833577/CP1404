f38b62c44a7dcac51e24874c3c7f406733a33509
Zhang Dawei <dawei.zhang@my.jcu.edu.au>
2025-10-14 12:05:34 +0800

    Add code to define the color dictionary and prompt the user for input.

---
+++ Prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+
+# Define a constant dictionary (COLOUR_CODES)
+COLOUR_CODES = {
+    "aliceblue": "#f0f8ff",
+    "antiquwhite": "#faebd7",
+    "aqua": "#00ffff",
+    "aquamarine": "#7fffd4",
+    "azure": "#f0ffff",
+    "beige": "#f5f5dc",
+    "bisque": "#ffe4c4",
+    "black": "#000000",
+    "blanchedalmond": "#ffencd",
+    "blue": "#0000ff"
+}
+
+# Prompt for input
+colour_name = input("Enter a colour name: ").lower()
