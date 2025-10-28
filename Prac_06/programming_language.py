2c46b9cf5524143a633540d78356e275545f1623
Zhang Dawei <dawei.zhang@my.jcu.edu.au>
2025-10-28 11:57:47 +0800

    Complete ProgrammingLanguage class with is_dynamic and __str__; list dynamic languages

--- Prac_06/programming_language.py
+++ Prac_06/programming_language.py
@@ -7,8 +7,15 @@ Actual:    minutes
 
 class ProgrammingLanguage:
     """Represent a programming language."""
-    def __int__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        """Return True if the language is dynamically typed."""
+        return self.typing.lower() == "dynamic"
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Refletion={self.reflection}, First appeared in {self.year}"
