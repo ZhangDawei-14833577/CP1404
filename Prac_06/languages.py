1a60e9f0ad9fef2912afadaa95d60d12e9ef731d
Zhang Dawei <dawei.zhang@my.jcu.edu.au>
2025-10-28 11:37:21 +0800

    Add initial framework for ProgrammingLanguage class and client code

---
+++ Prac_06/languages.py
@@ -0,0 +1,14 @@
+"""
+Client code to use ProgrammingLanguage
+
+Estimate: 25 minutes
+Actual:  minutes
+"""
+from programming_language import ProgrammingLanguage
+
+def mamin():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
---
+++ Prac_06/programming_language.py
@@ -0,0 +1,14 @@
+"""
+Programming Language class
+
+Estimate: 20 minutes
+Actual:    minutes
+"""
+
+class ProgrammingLanguage:
+    """Represent a programming language."""
+    def __int__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
