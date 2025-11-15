4876709b90c4e7741319fa464d5528bcecf98e16
Zhang Dawei <dawei.zhang@my.jcu.edu.au>
2025-11-15 10:52:28 +0800

    add Clear handler to reset input and label

--- Prac_08/box_layout_demo.py
+++ Prac_08/box_layout_demo.py
@@ -12,4 +12,7 @@ class BoxLayoutDemo(App):
         name = self.root.ids.input_name.text.strip()
         self.root.ids.output_label.text = f"Hello {name}" if name else "Hello"
 
+    def handle_clear(self):
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
 BoxLayoutDemo().run()
