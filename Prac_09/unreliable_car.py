a81601d4e304bbd0ec2d3c5b3c35110bdd4d991b
Zhang Dawei <dawei.zhang@my.jcu.edu.au>
2025-11-23 10:33:39 +0800

    Add UnreliableCar class skeleton with __init__

---
+++ Prac_09/unreliable_car.py
@@ -0,0 +1,23 @@
+"""UnreliableCar class that inherits from Car."""
+
+from random import randint
+from car import Car
+
+
+class UnreliableCar(Car):
+    """Specialised Car that only sometimes drives, based on reliability."""
+
+    def __init__(self, name: str, fuel: float, reliability: float):
+        """Initialise an UnreliableCar with name, fuel and reliability percentage."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance: float) -> float:
+        """Attempt to drive the given distance.
+
+        Drive only if a random number (0-100) is less than reliability.
+        Return the distance actually driven (0 or the normal Car.drive result).
+        """
+
+        return 0.0
+
